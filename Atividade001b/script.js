490569a96d01a53d179d36ee36189049d7e586c1
emanoel-maffei <emanoel.maffei@escola.pr.gov.br>
2025-06-18 17:34:59 -0300

    Add files via upload

---
+++ Atividade001b/script.js
@@ -0,0 +1,9 @@
+/* JavaScript da Atividade001 da parte JS */
+
+let nome = "Emanoel";
+let idade = 15;
+let podeDirigir = false;
+
+console.log(typeof(nome));
+console.log(typeof(idade));
+console.log(typeof(podeDirigir));
